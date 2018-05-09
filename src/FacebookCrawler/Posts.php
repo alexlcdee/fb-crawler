1c1a2ee2bbac737b053f754c5c819d3187f007a7
alexlcdee <info@alexlcdee.ru>
2018-05-09 20:20:19 +0300

    parse reactions

--- src/FacebookCrawler/Posts.php
+++ src/FacebookCrawler/Posts.php
@@ -4,6 +4,7 @@ namespace App\FacebookCrawler;
 
 
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Psr7\Uri;
 use Symfony\Component\DomCrawler\Crawler;
 
 class Posts
@@ -56,7 +57,7 @@ class Posts
     {
         return $this->retrievePosts(
             $url,
-            '#structured_composer_async_container .dj.dk.dl',
+            '#structured_composer_async_container .dg.dh.di > .dj.dk.dl',
             [$this, 'retrieveRecentPosts']
         );
     }
@@ -75,8 +76,16 @@ class Posts
 
         $page = new Crawler($body);
 
-        $posts = $page->filter($postSelector)->each(function (Crawler $node) {
-            return $node->text();
+        $posts = $page->filter($postSelector)->each(function (Crawler $node) use ($postSelector) {
+            $authorNode = $node->filter('table h3 a');
+            $authorUrl = new Uri($authorNode->first()->attr('href'));
+
+            return [
+                'authorLink' => $authorUrl->getPath(),
+                'authorName' => $authorNode->first()->text(),
+                'content'    => $node->filter("div > div")->eq(2)->text(),
+                'reactions'  => $this->crawlReactions($node),
+            ];
         });
 
         $nextPage = $page->filter('#structured_composer_async_container > .g > a');
@@ -88,6 +97,89 @@ class Posts
         }
 
         return $posts;
+    }
+
+    /**
+     * @param Crawler $postNode
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    private function crawlReactions(Crawler $postNode)
+    {
+        $comments = [];
+        $likes = [];
+        $reactionsContainer = $postNode->filter('.dt, .co, .cu, .cv, .cg, .bq, .cg');
+        $commentPageLink = $reactionsContainer->filter('a')->reduce(function (Crawler $node) {
+            return stripos($node->text(), 'comment') !== false;
+        });
+        if ($commentPageLink->count()) {
+            $url = $commentPageLink->attr('href');
+            $response = $this->client->request('GET', $url);
+            $commentsPage = new Crawler($response->getBody()->getContents());
+            $commentsContainer = $commentsPage->filter('div')->reduce(function (Crawler $node) {
+                return stripos($node->attr('id'), 'ufi_') !== false;
+            });
+            if ($commentsContainer->count()) {
+                $comments = $this->crawlComments($commentsPage->first());
+                $likes = $this->crawlLikes($commentsPage->first());
+            }
+//            file_put_contents(__DIR__.'/../../www/pages/'.md5($url).'.html', $response->getBody()->getContents());
+        }
+
+        return [
+            'comments' => $comments,
+            'likes'    => $likes,
+        ];
+    }
+
+    /**
+     * @param Crawler $commentsContainer
+     * @return array
+     */
+    private function crawlComments(Crawler $commentsContainer)
+    {
+        return $commentsContainer->filter('div > div > div')
+            ->reduce(function (Crawler $comment) {
+                $id = $comment->attr('id');
+
+                return $id === preg_replace('/\D/', '', $id);
+            })
+            ->each(function (Crawler $comment) {
+                $nameNode = $comment->filter('h3 a')->first();
+                $textNode = $comment->filter('div > div > div')->first();
+
+                return [
+                    'commentatorName' => $nameNode->count() ? $nameNode->text() : '',
+                    'commentatorUrl'  => $nameNode->count() ? (new Uri($nameNode->attr('href')))->getPath() : '',
+                    'comment'         => $textNode->count() ? $textNode->text() : '',
+                ];
+            });
+    }
+
+    /**
+     * @param Crawler $commentsPage
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    private function crawlLikes(Crawler $commentsPage)
+    {
+        $reactionsContainer = $commentsPage->filter('div')->reduce(function (Crawler $node) {
+            return stripos($node->attr('id'), 'sentence_') !== false;
+        });
+        if ($reactionsContainer->filter('a')->count()) {
+            $response = $this->client->request('GET', $reactionsContainer->filter('a')->first()->attr('href'));
+            $body = $response->getBody()->getContents();
+            $reactionsPage = new Crawler($body);
+
+            return $reactionsPage->filter('h3 a')->each(function (Crawler $reaction) {
+                return [
+                    'link' => $reaction->attr('href'),
+                    'name' => $reaction->text(),
+                ];
+            });
+        }
+
+        return [];
     }
 
     /**
